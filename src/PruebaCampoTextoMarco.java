fe4bf1d93aef29a7ce0df28fa11c7d05bb406d8a
nayus <nadiaopez12384@gmail.com>
2022-10-12 10:20:53 -0500

    holi

---
+++ src/PruebaCampoTextoMarco.java
@@ -0,0 +1,13 @@
+// Fig. 12.10: PruebaCampoTextoMarco.java
+// Prueba de CampoTextoMarco.
+
+import javax.swing.JFrame;
+
+public class PruebaCampoTextoMarco {
+    public static void main(String[] args) {
+        CampoTextoMarco campoTextoMarco = new CampoTextoMarco();
+        campoTextoMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        campoTextoMarco.setSize(350, 100);
+        campoTextoMarco.setVisible(true);
+    }
+} // fin de la clase PruebaCampoTexto}
